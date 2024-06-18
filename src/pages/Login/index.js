0c00b204599bbe54da98ffd69c5ee5fc652b3b48
leo-prata <leo.prata@estudante.ufjf.br>
2024-06-18 14:42:37 -0300

    setting up axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
 
-export default function Login(){
+export default function Login(){  
     return (
         <Container>
         <Title>
