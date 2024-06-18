58c64bb905e75783c746760957b92c1b8d2b619e
leo-prata <leo.prata@estudante.ufjf.br>
2024-06-18 16:02:21 -0300

    initial redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Nav } from './styled';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header(){
+    const buttonClicked = useSelector(state => state.buttonClick); // " state.buttonClick " vindo do Store  
     return(
         <Nav>
             <Link to='/'>Home</Link>
             <Link to='/teste'>Teste</Link>
+            {buttonClicked ? 'Clicked' : 'Not Clicked'}
         </Nav>    
     );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux'
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
 
 export default function Login(){  
+    const dispatch = useDispatch();
+    
+    function handleClick(e){
+        e.preventDefault();
+        dispatch({
+            type: 'BUTTON_CLICK',
+        });
+    }
+    
     return (
         <Container>
         <Title>
             Login
         </Title>
+        <br />
+        <button type='button' onClick={handleClick}>
+            Enviar
+        </button>
         </Container>
     );
 }
