d63c25f9c1354df6a4ee4a38800aee8dfd7d3812
leo-prata <leo.prata@estudante.ufjf.br>
2024-06-20 14:18:30 -0300

    add pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Nav } from './styled';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 export default function Header(){
-    const buttonClicked = useSelector(state => state.example.buttonClick); // " state.buttonClick " vindo do Store  
     return(
         <Nav>
             <Link to='/'>Home</Link>
-            <Link to='/teste'>Teste</Link>
-            {buttonClicked ? 'Clicked' : 'Not Clicked'}
+            <Link to='/register'>Register</Link>
+            <Link to='/login'>Login</Link>
         </Nav>    
     );
 }
